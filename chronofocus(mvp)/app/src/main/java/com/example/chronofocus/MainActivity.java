0e3fb49b0eb1e59309bd5f004695baf9b1d85d0c
aryadneSC <aryadne.campos@ba.estudante.senai.br>
2026-06-09 16:47:45 -0300

    feat: add telas de login simples, home e de add matéria

--- chronofocus(mvp)/app/src/main/java/com/example/chronofocus/MainActivity.java
+++ chronofocus(mvp)/app/src/main/java/com/example/chronofocus/MainActivity.java
@@ -24,25 +24,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         edtNovaMateria = findViewById(R.id.edtNovaMateria);
         btnAdicionar = findViewById(R.id.btnAdicionarMateria);
         lvMaterias = findViewById(R.id.listVMaterias);
 
-        btnAdicionar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Matéria Adicionada!", Toast.LENGTH_SHORT).show();
+        Button btnVoltar = findViewById(R.id.btnVoltarMateria);
+        btnVoltar.setOnClickListener(v -> finish());
 
+        btnAdicionar.setOnClickListener(v -> {
+            String nomeMateria = edtNovaMateria.getText().toString().trim();
+
+            if (nomeMateria.isEmpty()) {
+                edtNovaMateria.setError("Insira o nome da matéria!");
+                edtNovaMateria.requestFocus();
+            } else {
+                Toast.makeText(MainActivity.this, "Matéria adicionada!", Toast.LENGTH_SHORT).show();
                 edtNovaMateria.setText("");
             }
         });
 
     }
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
 }
