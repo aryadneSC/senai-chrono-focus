00a127540d61f8a4520a8aaa7b177721769035d3
Uslyst <147053055+Uslyst@users.noreply.github.com>
2026-06-13 16:43:35 -0300

    Atualizações no Front e preparação dos botões de SessionActivity
    - Renomeou activity_timer.xml para activity_session.xml
    - Adicionou TextView "textView_Materia" em activity_session.xml para exibir a matéria atual do timer
    - Adicionou botão de voltar na tela de sessão. Falta implementar.
    - Implementação de FlexboxLayout para organizar os botões de iniciar, finalizar e pausar.
    - Implementou layout e design de ActivitySession
    - Adicionou paleta de cores e criou temas para o aplicativo: modo claro e escuro que são selecionados automaticamente de acordo com a configuração do app

--- chronofocus-mvp/app/src/main/java/com/example/chronofocus/activities/SessionActivity.java
+++ chronofocus-mvp/app/src/main/java/com/example/chronofocus/activities/SessionActivity.java
@@ -3,6 +3,8 @@ package com.example.chronofocus.activities;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,21 +14,21 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.chronofocus.R;
-import com.example.chronofocus.databinding.ActivityTimerBinding;
-import com.example.chronofocus.model.DaysWeek;
+import com.example.chronofocus.databinding.ActivitySessionBinding;
 import com.example.chronofocus.model.Materia;
+import com.example.chronofocus.model.SessionTimer;
 import com.example.chronofocus.utils.TimerUtils;
 import com.example.chronofocus.viewmodels.SessionViewModel;
 
 public class SessionActivity extends AppCompatActivity {
-    private ActivityTimerBinding binding;
+    private ActivitySessionBinding binding;
     private SessionViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        binding = ActivityTimerBinding.inflate(getLayoutInflater());
+        binding = ActivitySessionBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         viewModel = new ViewModelProvider(this, ViewModelProvider.Factory.from(SessionViewModel.initializer)).get(SessionViewModel.class);
         setContentView(view);
@@ -44,25 +46,33 @@ public class SessionActivity extends AppCompatActivity {
             instancia até que o countdown chegue a zero, causando
          um memory leak e sérios travamentos na MainThread(UI Thread) em situações de multiplos cliques.*/
 
-        viewModel.getNextMateria();
+        // TEMPORARIO
+        Materia proximaMateria = viewModel.getSessionTimer();
+        Button buttonStart = binding.btnStart;
+        Button buttonFinish = binding.btnFinish;
+        Button buttonPause = binding.btnPause;
+        binding.tvTitle.setText(R.string.sessao);
 
         long time = proximaMateria.getBaseTime();
-        binding.button.setOnClickListener(l -> {
+
+        binding.tvMateria.setText(proximaMateria.getNome());
+        binding.tvTimer.setText(TimerUtils.millisToFormattedTimeString(time));
+
+        buttonStart.setOnClickListener(l -> {
+            buttonStart.setVisibility(View.GONE);
+            buttonFinish.setVisibility(View.VISIBLE);
+            buttonPause.setVisibility(View.VISIBLE);
+
+
            new CountDownTimer(time, 1) {
                 @Override
                 public void onTick(long millisUntilFinished) {
-                    // talvez adicionar titulo da materia em cima
-                    binding.textViewTimer.setText(TimerUtils.millisToFormattedTimeString(millisUntilFinished));
+                    binding.tvTimer.setText(TimerUtils.millisToFormattedTimeString(millisUntilFinished));
                 }
 
                 @Override
                 public void onFinish() {
-                    // SessionViewModel.GetNextMateria(),
-                    // se conseguir, prossegue, se não, finish().
-                    // GetNextMateria() chama SessionManager
-                    //
-                    // Materia materia = SessionViewModel.GetNextMateria()
-                    // time = materia.getBaseTime();
+
                 }
             }.start();
         });
